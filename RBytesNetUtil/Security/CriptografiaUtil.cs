64f05c472a25ea42d60b6907d35a7d9961bdc9b6
Rodrigo Delphino <rodrigodelphino@hotmail.com>
2025-04-23 20:20:52 -0300

    inclusão de novos métodos na criptografiaUtil

--- RBytesNetUtil/Security/CriptografiaUtil.cs
+++ RBytesNetUtil/Security/CriptografiaUtil.cs
@@ -66,5 +66,75 @@ namespace RBytesNetUtil
             }
         }
 
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+            
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+    public static bool VerificarMd5Hash(MD5 md5Hash, string input, string hash)
+    {
+
+        string hashOfInput = GetMd5Hash(md5Hash, input);
+
+        StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+        if (0 == comparer.Compare(hashOfInput, hash))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+        public static string GerarHashMd5(string input)
+        {
+            MD5 md5Hash = MD5.Create();
+
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        public static string GerarSenha(int tamanho)
+        {
+            var chars = "abcdfghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(chars, tamanho)
+                        .Select(s => s[random.Next(s.Length)])
+                        .ToArray());
+            return result;
+        }
+
+        public static string GerarSenhaToken(int tamanho)
+        {
+            var chars = "!@#$%&*()-+={}[]abcdfghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(chars, tamanho)
+                        .Select(s => s[random.Next(s.Length)])
+                        .ToArray());
+            return result;
+        }
+
     }
 }
