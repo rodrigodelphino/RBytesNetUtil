38353a981e4283284d70ba9b9eab85957c05a55b
Rodrigo Delphino <rodrigodelphino@hotmail.com>
2025-10-20 13:20:37 -0300

    criação da classe para validações basicas dentro do nampespace util

---
+++ RBytesNetUtil/Util/VerificarCampoValido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RBytesNetUtil;
+
+public static class RBytesUtil
+{
+    public static bool ValorComboValido(string valor, string valorInvalido = "0")
+    {
+        if (string.IsNullOrEmpty(valor))
+            return false;
+
+        if (valor.Equals(valorInvalido))
+            return false;
+
+        return true;
+    }
+}
